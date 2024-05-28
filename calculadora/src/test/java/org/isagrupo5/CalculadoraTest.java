3a9ad9691215833b6783b3b45e82cf3cd6da5cae
Iván López Martínez. DMOS <ivan.lopezm@elecnor-deimos.com>
2024-05-28 15:59:30 +0200

    Añadidos los tests de multiplicación y simplificado código

--- calculadora/src/test/java/org/isagrupo5/CalculadoraTest.java
+++ calculadora/src/test/java/org/isagrupo5/CalculadoraTest.java
@@ -12,53 +12,76 @@ public class CalculadoraTest {
 
     @Test
     public void testSumaDouble(){
-        Double resultadoSuma = calculadoraTest.sumar(2.0, 4.0);
-        Assertions.assertEquals(6.0, resultadoSuma, "La suma de 2.0 y 4.0 debería ser 6.0");
+        Assertions.assertEquals(6.0, calculadoraTest.sumar(2.0, 4.0), "La suma de 2.0 y 4.0 debería ser 6.0");
     }
 
     @Test
     public void testSumaFloat(){
-        Double resultadoSuma = calculadoraTest.sumar(20f, 4.0f);
-        Assertions.assertEquals(24.0f, resultadoSuma, "La suma de 20f y 4.0f debería ser 24.0f");
+        Assertions.assertEquals(24.0f, calculadoraTest.sumar(20f, 4.0f), "La suma de 20f y 4.0f debería ser 24.0f");
     }
 
     @Test
     public void testSumaInt(){
-        Double resultadoSuma = calculadoraTest.sumar(2, 8);
-        Assertions.assertEquals(10, resultadoSuma, "La suma de 2 y 8 debería ser 10");
+        Assertions.assertEquals(10, calculadoraTest.sumar(2, 8), "La suma de 2 y 8 debería ser 10");
     }
 
     @Test
     public void testSumaPosNeg(){
-        Double resultadoSuma = calculadoraTest.sumar(-2.0, 8);
-        Assertions.assertEquals(6.0, resultadoSuma, "La suma de 2 y 8 debería ser 10");
+        Assertions.assertEquals(6.0, calculadoraTest.sumar(-2.0, 8), "La suma de 2 y 8 debería ser 10");
     }
 
     //Bloque resta
 
     @Test
     public void testRestaDouble(){
-        Double resultadoSuma = calculadoraTest.resta(2.0, 4.0);
-        Assertions.assertEquals(-2.0, resultadoSuma, "La resta de 2.0 y 4.0 debería ser -2.0");
+        Assertions.assertEquals(-2.0, calculadoraTest.resta(2.0, 4.0), "La resta de 2.0 y 4.0 debería ser -2.0");
     }
 
     @Test
     public void testRestaFloat(){
-        Double resultadoSuma = calculadoraTest.resta(20f, 4.0f);
-        Assertions.assertEquals(16.0f, resultadoSuma, "La resta de 20f y 4.0f debería ser 16.0f");
+        Assertions.assertEquals(16.0f, calculadoraTest.resta(20f, 4.0f), "La resta de 20f y 4.0f debería ser 16.0f");
     }
 
     @Test
     public void testRestaInt(){
-        Double resultadoSuma = calculadoraTest.resta(2, 8);
-        Assertions.assertEquals(-6, resultadoSuma, "La resta de 2 y 8 debería ser -6");
+        Assertions.assertEquals(-6, calculadoraTest.resta(2, 8), "La resta de 2 y 8 debería ser -6");
     }
 
     @Test
     public void testRestaPosNeg(){
-        Double resultadoSuma = calculadoraTest.resta(2.0, -8);
-        Assertions.assertEquals(10.0, resultadoSuma, "La resta de 2.0 y -8 debería ser 10");
+        Assertions.assertEquals(10.0, calculadoraTest.resta(2.0, -8), "La resta de 2.0 y -8 debería ser 10");
     }
+
+    //Bloque multiplicación
+
+    @Test
+    public void testMultiplicacionPosNeg(){
+        Assertions.assertEquals(-16.0, calculadoraTest.multiplicacion(2.0, -8), "La multiplicación de 2.0 y -8 debería ser -16");
+    }
+
+    @Test
+    public void testMultiplicacionInt(){
+        Assertions.assertEquals(10, calculadoraTest.multiplicacion(2, 5), "La multiplicación de 2 y 5 debería ser 10");
+    }
+
+    @Test
+    public void testMultiplicacionDouble(){
+        Assertions.assertEquals(10.5, calculadoraTest.multiplicacion(2.5, 4.2), "La multiplicación de 2.5 y 4.2 debería ser 10.5");
+    }
+
+    @Test
+    public void testMultiplicacionFloat(){
+        Assertions.assertEquals(80.0f, calculadoraTest.multiplicacion(20f, 4.0f), "La multiplicación de 20f y 4.0f debería ser 80.0f");
+    }
+
+
+
+
+
+
+
+
+
 
 
 
